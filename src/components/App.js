ecfc60594c82ae4be18a8a7276ca8257b2696ecb
Rahul <gupta-9631>
2022-08-26 01:02:52 +0530

    fix

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,24 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+	    this.state = {
+		para: false
 	};
-
+	}
+	
+	para(){
+		return(
+		<p id="para">
+			Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+			</p>
+			);
+	}
+	
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+			<button id="click" onClick={()=> this.setState({para: true})}>Click</button>
+{this.state.para ? this.para() : ""}
     		</div>
     	);
     }
@@ -17,4 +29,3 @@ class App extends Component {
 
 
 export default App;
-
